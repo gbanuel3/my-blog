2fc55b7917363f9857de2673177211e2c7355dab
Gil Banuelos <gbanuel3@gmail.com>
2024-03-21 05:05:11 -0500

    db setup

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
-const { Pool } = require('pg')
+require('dotenv').config();
+const { Pool } = require('pg');
 
 const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
-let DB_HOST
+
+let pool;
 if (process.env.NODE_ENV === 'production') {
-  DB_HOST = `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+  pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+    // Remove port and SSL when connecting through Unix socket
+  });
 } else {
-  DB_HOST = process.env.DB_IP
+  pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_IP,
+    port: process.env.DB_PORT, // Specify the port if needed for local development
+    // SSL configuration for local development if needed
+  });
 }
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
-
-module.exports = pool
+module.exports = pool;
